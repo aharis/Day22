43f596d0d89d15610c65738f2984633755b3af99
aharis <hare.arapovic@gmail.com>
2021-03-29 21:33:36 +0200

    dodao zadatak ex03

---
+++ ex03/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Plant from './Plant';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Plant">
+      <Plant />
+    </div>
+  );
+}
+
+export default App;
